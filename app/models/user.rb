c4819bda021df9eb93def9f0e0814946ec93618d
cirarcts <komomasa1350727@gmail.com>
2023-03-25 12:30:26 +0900

    userモデルのバリデーション設定〜deviseビュー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :buys
+
+  validates :nickname, :last_name, :first_name,
+            :last_name_ruby, :first_name_ruby,
+            :date_of_birth, presence:true
 end
