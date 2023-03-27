e43ede8a7adf2d8bd98d79d9c0715c1acc94ccd6
cirarcts <komomasa1350727@gmail.com>
2023-03-27 16:25:15 +0900

    各カラムのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :buys
 
-  validates :nickname, :last_name, :first_name,
-            :last_name_ruby, :first_name_ruby,
-            :date_of_birth, presence:true
+  validates :nickname, :date_of_birth, presence:true
+            # 各カラムの値が空では値の保存を受け付けない
+
+  validates :password, presence: true, length: { minimum: 6 } , format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+            # パスワードは6文字以上、半角英数字を含む入力必須
+
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+            # 全角（漢字・平仮名・片仮名でのいずれか）の入力必須
+
+  validates :last_name_ruby, :first_name_ruby, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+            # 全角片仮名での入力必須
 end
