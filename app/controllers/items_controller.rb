9f0d509b1135982f6b2972edbe3e1c1d388d5199
cirarcts <komomasa1350727@gmail.com>
2023-04-02 14:06:25 +0900

    ActiveHash IDの表示設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,23 @@
 class ItemsController < ApplicationController
   def index
-    @items = item.order("created_at DESC")
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:product_name, :image),merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
