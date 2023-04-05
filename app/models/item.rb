d26e925a12fafd5f069c775c72df772e345a05b4
cirarcts <komomasa1350727@gmail.com>
2023-04-05 23:52:10 +0900

    バリデーション機能時のエラーメッセージ表示機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,13 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   #空の状態では値を保存しない
-  validates :product_name, :explanation, :image, presence: true
-
-  #数値が300〜9,999,999の範囲かつ、半角数値でなければ値を保存しない
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :image, :product_name, :explanation, presence: true
 
   #各カラムの選択が「---」の時は値を保存しない
   validates :category_id, :condition_id, :shipping_cost_id,
             :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+  #数値が300〜9,999,999の範囲かつ、半角数値でなければ値を保存しない
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
 end
