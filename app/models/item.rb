60d9bf65267cd053233a29f494097eb1ea221e7a
cirarcts <komomasa1350727@gmail.com>
2023-04-05 23:23:52 +0900

    商品情報の保存機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   #空の状態では値を保存しない
-  validates :product_name, :explanation, :price, :image, presence: true
+  validates :product_name, :explanation, :image, presence: true
+
+  #数値が300〜9,999,999の範囲かつ、半角数値でなければ値を保存しない
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
 
   #各カラムの選択が「---」の時は値を保存しない
   validates :category_id, :condition_id, :shipping_cost_id,
