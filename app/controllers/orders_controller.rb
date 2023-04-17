02fc10c91789ee59341850613efd2cfb08583a6d
cirarcts <komomasa1350727@gmail.com>
2023-04-18 04:31:04 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    
+    @order_shipping = Item.find(params[:item_id])
+    @order = OrderShipping.new(item_id: @order_shipping.id)
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      pay_item
+      @order_shipping.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:item_id, :user_id, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    item = Item.find(params[:item_id])
+    Payjp::Charge.create(
+      amount: item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
---
+++ app/models/order_shipping.rb
@@ -0,0 +1,19 @@
+class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :token, :item_id, :user_id, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :token, :item_id, :user_id, :municipalities, :house_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    # 購入情報を保存し、変数orderに代入する
+    order = Order.create(item_id: item_id, user_id: user_id)
+    # 住所を保存する
+    # order_idには、変数orderのidと指定する
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
