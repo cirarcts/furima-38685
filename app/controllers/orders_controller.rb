9a6e890a5284a976b7a34122ee5a7b0691acc29a
cirarcts <komomasa1350727@gmail.com>
2023-04-18 14:19:14 +0900

    コード整形

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
     if @order.valid?
       pay_item
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
@@ -25,15 +25,18 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping).permit(:item_id, :user_id, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_shipping).permit(:item_id, :user_id, :postal_code, :prefecture_id,
+                                           :municipalities, :house_number, :building_name, :phone_number).merge(
+                                             token: params[:token], item_id: params[:item_id], user_id: current_user.id
+                                           )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # PAY.JPテスト秘密鍵
     item = Item.find(params[:item_id])
     Payjp::Charge.create(
-      amount: item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      amount: item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
