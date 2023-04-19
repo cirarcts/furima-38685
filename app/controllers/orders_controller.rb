d026a57e9a0fe50e407610f9c3f81ad235ba3184
cirarcts <komomasa1350727@gmail.com>
2023-04-19 20:18:04 +0900

    コード整形

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,9 +24,10 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(
-      token: params[:token], user_id: current_user.id, item_id: params[:item_id]
-    )
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id,
+                                           :municipalities, :house_number, :building_name, :phone_number).merge(
+                                             token: params[:token], user_id: current_user.id, item_id: params[:item_id]
+                                           )
   end
 
   def set_order_shipping
